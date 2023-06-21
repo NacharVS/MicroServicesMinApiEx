80ded488141fde6490dada988c0cf3b967b8848d
nacharvs <r3dinc@yandex.ru>
2023-06-22 01:48:48 +0300

    start

---
+++ MicroServises/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MicroServises.UserService.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        public ObjectId MyProperty { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+    }
+}
