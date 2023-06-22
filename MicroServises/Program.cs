8591a9853f311606a11516f2fdd2d5d26028033f
nacharvs <r3dinc@yandex.ru>
2023-06-22 09:00:33 +0300

    UseExceptionPage

--- MicroServises/Program.cs
+++ MicroServises/Program.cs
@@ -1,19 +1,41 @@
 using MicroServises.UserService.Infrastructure;
 using MicroServises.UserService.Models;
 
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
+
+app.UseDeveloperExceptionPage();
+
+
+
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 
 app.MapGet("/", () => "Hello");
+
 app.MapGet("/getUser/{login}", (string login) => MongoDBContext.GetByName(login));
-app.MapPost("/sendUser", (User user) => MongoDBContext.Add(user));
+
+app.MapPost("/sendUser", (User user) => {
+    MongoDBContext.Add(user);
+    
+    });
+app.Run(async (context) =>
+{
+    int[]qq = new int[10];
+    for (int i = 0; i < 20; i++)
+    {
+        qq[i] = 1;
+    }
+});
 app.Run();
